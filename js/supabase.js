2d3a4bebc4780c84e8c2b08ff488ed7016ee8da0
LiSenokYT <131493918+LiSenokYT@users.noreply.github.com>
2025-11-10 18:44:59 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,6 @@
-// supabase.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const supabaseUrl = 'https://taiyivtpciubzfiefgwa.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhaXlpdnRwY2l1YnpmaWVmZ3dhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3MDg3NDIsImV4cCI6MjA3ODI4NDc0Mn0.WEAcGv11HEFQccncXM12Dtes2j9nL4vEf31_a6Frh4Y'
+const supabaseUrl = 'https://your-project.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhaXlpdnRwY2l1YnpmaWVmZ3dhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3MDg3NDIsImV4cCI6MjA3ODI4NDc0Mn0.WEAcGv11HEFQccncXM12Dtes2j9nL4vEf31_a6Frh4Y'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
