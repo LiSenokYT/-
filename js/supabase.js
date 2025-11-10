a8f8dbac429403decc7668bdbf0aec998c8ab21e
LiSenokYT <131493918+LiSenokYT@users.noreply.github.com>
2025-11-10 17:10:44 +0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
+// supabase.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-// ЗАМЕНИ ЭТИ ЗНАЧЕНИЯ НА СВОИ:
 const supabaseUrl = 'https://taiyivtpciubzfiefgwa.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhaXlpdnRwY2l1YnpmaWVmZ3dhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3MDg3NDIsImV4cCI6MjA3ODI4NDc0Mn0.WEAcGv11HEFQccncXM12Dtes2j9nL4vEf31_a6Frh4Y'
 
